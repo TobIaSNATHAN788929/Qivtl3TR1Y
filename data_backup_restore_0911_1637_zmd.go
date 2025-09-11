a3e62eb065f04ffdb7349efd87c5eb527058085e
TobIaSNATHAN788929 <plops-retails-0g@icloud.com>
2025-09-11 09:37:24 +0100

    Create 数据备份恢复

---
+++ data_backup_restore_0911_1637_zmd.go
@@ -0,0 +1,17 @@
+// 代码生成时间: 2025-09-11 16:37:23
+package main
+
+import (
+    "crypto/md5"
+    "encoding/hex"
+    "fmt"
+    "io"
+    "io/ioutil"
+    "log"
+    "os"
+    "os/exec"
+    "path/filepath"
+    "strings"
+    "time"
+
+    
